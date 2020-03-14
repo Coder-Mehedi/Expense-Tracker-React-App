dc609c0637295c878b15932bd68a3726cf14f661
Coder-Mehedi <mehedi5051@gmail.com>
2020-03-14 20:39:49 +0600

    prepaired build for production

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
@@ -17,6 +18,11 @@ if(process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/transactions', transactions)
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server Running In ${process.env.NODE_ENV} Mode On Port ${PORT}`.america.bold))
